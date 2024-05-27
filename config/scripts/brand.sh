2c94da81b8c6c4b21aa706ff6d997348721649e2
christoph00 <christoph@asche.co>
2024-05-27 15:22:17 +0200

    ok

--- config/scripts/brand.sh
+++ config/scripts/brand.sh
@@ -2,9 +2,5 @@
 
 set -ouex pipefail
 
-# Branding for Bluefin/Aurora
-if test "$BASE_IMAGE_NAME" = "silverblue"; then
-    sed -i '/^PRETTY_NAME/s/Silverblue/caos/' /usr/lib/os-release
-elif test "$BASE_IMAGE_NAME" = "kinoite"; then
-    sed -i '/^PRETTY_NAME/s/Kinoite/caos/' /usr/lib/os-release
-fi
+sed -i '/^PRETTY_NAME/s/Kinoite/caos/' /usr/lib/os-release
+
