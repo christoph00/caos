e3c567261eb89f9bbf418a5301e153dec090e543
christoph00 <christoph@asche.co>
2024-05-27 15:10:58 +0200

    ok

--- config/scripts/base.sh
+++ config/scripts/base.sh
@@ -3,4 +3,3 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_MAJOR_VERSION}"/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
