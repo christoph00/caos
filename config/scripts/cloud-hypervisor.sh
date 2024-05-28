1ecd6b89d16eac408358efc0de06daee302b51f7
christoph00 <christoph@asche.co>
2024-05-28 08:26:12 +0200

    add ch-remote

--- config/scripts/cloud-hypervisor.sh
+++ config/scripts/cloud-hypervisor.sh
@@ -6,3 +6,9 @@ set -oue pipefail
 LATEST_URL=$(curl -sL https://api.github.com/repos/cloud-hypervisor/cloud-hypervisor/releases/latest | jq -r '.assets[] | select(.name? | match(".*cloud-hypervisor$")) | .browser_download_url')
 curl -sL -o /usr/bin/cloud-hypervisor ${LATEST_URL}
 chmod +x /usr/bin/cloud-hypervisor
+
+
+# CH Remote
+LATEST_REMOTE_URL=$(curl -sL https://api.github.com/repos/cloud-hypervisor/cloud-hypervisor/releases/latest | jq -r '.assets[] | select(.name? | match(".*ch-remote$")) | .browser_download_url')
+curl -sL -o /usr/bin/ch-remote ${LATEST_REMOTE_URL}
+chmod +x /usr/bin/ch-remote
