2f2d78a24e85a183a113634aaffb871446a9db94
christoph00 <christoph@asche.co>
2024-05-28 08:24:38 +0200

    add cloud-hypervisor

---
+++ config/scripts/cloud-hypervisor.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+LATEST_URL=$(curl -sL https://api.github.com/repos/cloud-hypervisor/cloud-hypervisor/releases/latest | jq -r '.assets[] | select(.name? | match(".*cloud-hypervisor$")) | .browser_download_url')
+curl -sL -o /usr/bin/cloud-hypervisor ${LATEST_URL}
+chmod +x /usr/bin/cloud-hypervisor
