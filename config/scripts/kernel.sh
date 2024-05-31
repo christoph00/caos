03f04fb0df477320f016b28723aa2b25e1afb33a
christoph00 <christoph@asche.co>
2024-06-01 00:46:47 +0200

    ok

--- config/scripts/kernel.sh
+++ config/scripts/kernel.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+# Tell this script to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree cliwrap install-to-root /
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto
+FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
+INSTALLED_KERNEL_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^kernel(?!-tools).*')"
+
+printf "### Fedora version ###\n$FEDORA_VERSION\n\n"
+printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n\n"
+sleep 2
+
+# Use rpm-ostree's cliwrap to allow dracut to run on the container and generate
+# an initramfs.
+### COMMENT OUT BELOW LINE IF USING LOCALLY ###
+rpm-ostree cliwrap install-to-root / && \
+rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-lto
+
+# Credit https://github.com/JosSamLoh/replace-kernel/blob/live/scripts/replace-kernel-easy.sh
