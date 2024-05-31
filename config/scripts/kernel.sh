ae5a732c67dde697d47b56365cc956c67516fd1f
christoph00 <christoph@asche.co>
2024-06-01 00:16:26 +0200

    ok

--- config/scripts/kernel.sh
+++ config/scripts/kernel.sh
@@ -2,3 +2,5 @@
 
 set -oue pipefail
 
+rpm-ostree cliwrap install-to-root /
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto
