6ce3563806793d02a7802a4456c54b55afc5581f
christoph00 <christoph@asche.co>
2024-06-03 17:08:34 +0200

    ok

--- config/scripts/kiss-vm.sh
+++ config/scripts/kiss-vm.sh
@@ -5,5 +5,10 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
+rpm-ostree install bash-completion libvirt libvirt-client virt-install virt-viewer qemu-kvm expect nmap-ncat tmux libguestfs-tools-c libvirt-nss dialog qemu-img udisks2 genisoimage iptables-nft ipcalc
 cd /tmp
-curl -s https://raw.githubusercontent.com/tcler/kiss-vm-ns/master/utils/kiss-update.sh | bash && vm prepare
+
+git clone https://github.com/tcler/kiss-vm-ns &&
+    make -C kiss-vm-ns && vm prepare
+
+rpm-ostree commit
